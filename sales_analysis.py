c7d4b53e14be00584225e444f8041135c442ad18
Matthias Holdorf <matthias.holdorf@gmail.com>
2020-10-18 19:02:52 +0200

    Add poking into data set

--- sales_analysis.py
+++ sales_analysis.py
@@ -1,5 +1,12 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-df = pd.read_csv('/KAG_conversion_data.csv')
-df.head()
+dataset = pd.read_csv("KAG_conversion_data.csv")
+
+print(dataset.head())
+print(dataset.info())
+print(dataset.shape)
+print(dataset.describe())
+
+print("We have customers from age groups as follows:")
+print(dataset['age'].unique())
